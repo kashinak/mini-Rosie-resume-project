df528269849c48028cea1b65ab5062ccec7e9091
kashinak <kashinakfx@gmail.com>
2021-06-25 19:43:12 +0000

    Implement emailjs integration on the contact page

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -1,5 +1,4 @@
-  
-function userInformationHTML(user) {
+ function userInformationHTML(user) {
     return `
         <h2>${user.name}
             <span class="small-name">
@@ -16,7 +15,30 @@ function userInformationHTML(user) {
         </div>`;
 }
 
+function repoInformationHTML(repos) {
+    if (repos.length == 0) {
+        return `<div class="clearfix repo-list">No repos!</div>`;
+    }
+
+    var listItemsHTML = repos.map(function(repo) {
+        return `<li>
+                    <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+                </li>`;
+    });
+
+    return `<div class="clearfix repo-list">
+                <p>
+                    <strong>Repo List:</strong>
+                </p>
+                <ul>
+                    ${listItemsHTML.join("\n")}
+                </ul>
+            </div>`;
+}
+
 function fetchGitHubInformation(event) {
+    $("#gh-user-data").html("");
+    $("#gh-repo-data").html("");
 
     var username = $("#gh-username").val();
     if (!username) {
@@ -29,20 +51,29 @@ function fetchGitHubInformation(event) {
             <img src="assets/css/loader.gif" alt="loading..." />
         </div>`);
 
-        $.when(
-            $.getJSON(`https://api.github.com/users/${username}`)
-        ).then(
-            function(response) {
-                var userData = response;
-                $("#gh-user-data").html(userInformationHTML(userData));
-            }, function(errorResponse) {
-                    if (errorResponse.status === 404) {
-                        $("#gh-user-data").html(`<h2>No info found for user ${username}</h2>`);
-                    } else {
-                        console.log(errorResponse);
-                        $("#gh-user-data").html(
-                            `<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
-                    }
-                
-            });
+    $.when(
+        $.getJSON(`https://api.github.com/users/${username}`),
+        $.getJSON(`https://api.github.com/users/${username}/repos`)
+    ).then(
+        function(firstResponse, secondResponse) {
+            var userData = firstResponse[0];
+            var repoData = secondResponse[0];
+            $("#gh-user-data").html(userInformationHTML(userData));
+            $("#gh-repo-data").html(repoInformationHTML(repoData));
+        },
+        function(errorResponse) {
+            if (errorResponse.status === 404) {
+                $("#gh-user-data").html(
+                    `<h2>No info found for user ${username}</h2>`);
+            } else if (errorResponse.status === 403) {
+                var resetTime = new Date(errorResponse.getResponseHeader('X-RateLimit-Reset') * 1000);
+                $("#gh-user-data").html(`<h4>Too many requests, please wait until ${resetTime.toLocaleTimeString()}</h4>`);
+            } else {
+                console.log(errorResponse);
+                $("#gh-user-data").html(
+                    `<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
+            }
+        });
 }
+
+$(document).ready(fetchGitHubInformation);
